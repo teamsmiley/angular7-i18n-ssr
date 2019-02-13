a5f48ee36848c7d580867ca9be15ad85e3ebd382
smiley <teamsmiley@gmail.com>
2019-02-13 08:32:52 -0800

    environment spa만 동작 ssr은 에러

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  isProduction: boolean = environment.production;
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
